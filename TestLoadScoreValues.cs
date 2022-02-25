0ae4e0080e152be41afdc9af49ca1c30fe1876c4
atkim2022 <atkim2022@chadwickschool.org>
2022-02-25 09:10:37 -0800

    tests

--- TestLoadScoreValues.cs
+++ TestLoadScoreValues.cs
@@ -7,7 +7,21 @@ namespace PersonalProject2
     {
         public static bool RunTest()
         {
+
+            // try and catch for a file that doesn't exist
+            // then making sure the values in the file are the things that are being returned
+        try
+        {
+            Program.LoadFile("NotAFile.txt");
+            Console.Error.WriteLine("Expected an exception from loading \"NotAFile.txt\"");
             return false;
+        }
+            
+        catch 
+        {
+            
+        }
+
         }
     }
 }
